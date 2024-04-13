7593ea3a1dd03b06bb7dd8b4268fc5b89686e4b8
arcana426 <164586096+arcana426@users.noreply.github.com>
2024-04-13 17:36:25 +0900

    Update message.js

--- en-us/JS/message.js
+++ en-us/JS/message.js
@@ -6,12 +6,12 @@ function respondToUser(comment) {
         aiResponse = 'You're welcome! If you have any more questions, don't hesitate to ask.';
     } else if (comment.includes('sleep')) {
         aiResponse = 'It looks like it's time for you to take a break! Have a good rest. I'll be here later if you have any more questions. Good night!';
-    } else if (comment.includes('どうしたの')) {
-        aiResponse = 'なんでもありません。';
-    } else if (comment.includes('おはよう')) {
-        aiResponse = 'おはようございます。';
-    } else if (comment.includes('こんばんは')) {
-        aiResponse = 'こんばんは!';
+    } else if (comment.includes('what's the matter?')) {
+        aiResponse = 'It's nothing.';
+    } else if (comment.includes('Good morning.')) {
+        aiResponse = 'Good morning.';
+    } else if (comment.includes('good evening')) {
+        aiResponse = 'Good evening!';
     } else if (comment.includes('chrononoteAI')) {
         aiResponse = '何かようですか？<br>ゆっくりしていってください。';
     } else if (comment.includes('hello')) {
